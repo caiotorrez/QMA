b6f183296a4cf83d56a65d565fc1ed383b348623
Caio Torrez <caiotorrez@live.com>
2018-02-22 23:14:42 -0300

    Caio Commit - 22/02 23:13 - Classes base, alteracao de nomes, etc

--- src/classePrincipal/Controller.java
+++ src/classePrincipal/Controller.java
@@ -5,15 +5,16 @@ import java.util.Map;
 
 import alunos.Aluno;
 import easyaccept.EasyAccept;
+import tutor.Disciplina;
+import tutor.Tutor;
 
 public class Controller {
+	
 
-	private Map<String, Aluno> alunos;
+	private Map<String, Aluno> alunos = new HashMap<>();
+	private Map<String, Tutor> tutores = new HashMap<>();
 
-	public void inicializar() {
-		this.alunos = new HashMap<>();
-	}
-
+	
 	public void cadastrarAluno(String nome, String matricula, int codigoCurso, String telefone, String email) {
 		this.alunos.put(matricula, new Aluno(nome, matricula, codigoCurso, telefone, email));
 	}
@@ -31,11 +32,27 @@ public class Controller {
 	}
 
 	public String getInfoAluno(String matricula, String atributo) {
-		return this.alunos.get(matricula).
+		return null;
+	}
+	
+	public void tornarTutor(String matricula, String disciplina, int proficiencia) {
+		this.tutores.put(matricula, new Tutor(this.alunos.get(matricula), new Disciplina(disciplina, proficiencia)));
+	}
+	
+	public String recuperaTutor(String matricula) {
+		return this.tutores.get(matricula).toString();
+	}
+	
+	public String listaTutores() {
+		String saida = "";
+		for (String matricula : this.tutores.keySet()) {
+			saida += this.tutores.get(matricula).toString() + System.lineSeparator();
+		}
+		return saida.equals("") ? "Nao ha tutores cadastrados" : saida;
 	}
 	
 	public static void main(String[] args) {
-		args = new String[] { "classePrincipal.Controller", "acceptance_test/us1_test.txt", "acceptance_test/us2_test.txt" };
+		args = new String[] { "classePrincipal.Controller", "testesAceitacao/us1_test.txt", "testesAceitacao/us2_test.txt" };
 		EasyAccept.main(args);
 	}
 
--- testes/alunosTeste/AlunoTeste.java
+++ testes/alunosTeste/AlunoTeste.java
@@ -2,14 +2,23 @@ package alunosTeste;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import alunos.Aluno;
 
 public class AlunoTeste {
-	private Aluno alunoTeste = new Aluno("Francis", "111222333", 45, "9999-9999", "francis@andrade.com");
-	private Aluno alunoTeste2 = new Aluno("Francis", "111222333", 45, "francis@andrade.com");
-
+	
+	private Aluno alunoTeste2;
+	private Aluno alunoTeste;
+	
+	
+	@Before
+	public void setUp() {
+		this.alunoTeste = new Aluno("Francis", "111222333", 45, "9999-9999", "francis@andrade.com");
+		this.alunoTeste2 = new Aluno("Francis", "111222333", 45, "", "francis@andrade.com");
+	}
+	
 	@Test
 	public void toStringComTelefoneTest() {
 		assertEquals("111222333 - Francis - 45 - 9999-9999 - francis@andrade.com", alunoTeste.toString());
