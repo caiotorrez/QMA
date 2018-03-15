aa638300e7bef8620588862a3fcc83f39a389a72
Caio Torrez <caiotorrez@live.com>
2018-03-14 23:01:27 -0300

    Adcionado javaDoc

--- src/servicos/ServiceHorarioLocais.java
+++ src/servicos/ServiceHorarioLocais.java
@@ -1,62 +1,102 @@
+/**
+ * UFCG - Universidade Federal de Campina Grande
+ * LP2 - Laboratorio de Programacao 2 - Projeto Final
+ * Caio Vitor Brasileiro Torres – 116111245
+ * Francivaldo Cabral de Andrade – 116111544
+ * Luan Carlos da Silva Bezerra - 116110100
+ */
 package servicos;
-
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
-
 import modelos.tempo.Horario;
 import modelos.tempo.Local;
-
+/**
+ * Classe ServiceCaixaSistema, realiza as operacoes no ambito do Caixa do Sistema
+ * @author Caio Torres
+ * @version 1.5
+ */
 @SuppressWarnings("serial")
 public class ServiceHorarioLocais implements Serializable {
-
-	private ServiceTutor serviceTutor;
-	private Map<String, Local> locaisAtendimento;
-	private Map<String, Horario> horarios;
-	
-	public ServiceHorarioLocais(ServiceTutor serviceTutor) {
-		this.serviceTutor = serviceTutor;
-		this.locaisAtendimento = new HashMap<>();
-		this.horarios = new HashMap<>();
-	}
-	
-	public void cadastrarHorario(String email, String horario, String dia) {
-		if (!this.serviceTutor.getAllTutores().contains(email)) {
-			throw new NullPointerException("Erro no cadastrar horario: tutor nao cadastrado");
-		} 
-		else if (this.horarios.containsKey(email)) {
-			this.addHorario(email, horario, dia);
-		} 
-		else {
-			this.horarios.put(email, new Horario(horario, dia));
-		}
-	}
-	
-	public void addHorario(String email, String horario, String dia) {
-		this.horarios.get(email).addHorario(horario, dia);
-	}
-
-	public boolean contemHorario(String email, String horario, String dia) {
-		if (this.horarios.containsKey(email) && this.horarios.get(email).contemDia(dia)) {
-			return this.horarios.get(email).getHorario(dia).equals(horario);
-		} return false;
-		
-	}
-
-	public void cadastrarLocal(String email, String local) {
-		if (!this.serviceTutor.getAllTutores().contains(email)) {
-			throw new NullPointerException("Erro no cadastrar local de atendimento: tutor nao cadastrado");
-		}
-		else if (this.locaisAtendimento.containsKey(email)) {
-			this.locaisAtendimento.get(email).addLocal(local);
-		} else {
-			this.locaisAtendimento.put(email, new Local(local));
-		}
-	}
-	
-	public boolean contemLocal(String email, String local) {
-		if (this.horarios.containsKey(email)) {
-			return this.locaisAtendimento.get(email).contemLocal(local);
-		} return false;
-	}
+    private ServiceTutor serviceTutor;
+    private Map<String, Local> locaisAtendimento;
+    private Map<String, Horario> horarios;
+    
+    /**
+     * Construtor de ServiceHorarioLocais
+     * @param serviceTutor, ServiceTutor
+     */
+    public ServiceHorarioLocais(ServiceTutor serviceTutor) {
+        this.serviceTutor = serviceTutor;
+        this.locaisAtendimento = new HashMap<>();
+        this.horarios = new HashMap<>();
+    }
+    
+    /**
+     * Cadastra um novo Horario livre para atendimento de um tutor cadastrado
+     * @param email, String
+     * @param horario, String
+     * @param dia, String
+     */
+    public void cadastrarHorario(String email, String horario, String dia) {
+        if (!this.serviceTutor.getAllTutores().contains(email)) {
+            throw new NullPointerException("Erro no cadastrar horario: tutor nao cadastrado");
+        } 
+        else if (this.horarios.containsKey(email)) {
+            this.addHorario(email, horario, dia);
+        } 
+        else {
+            this.horarios.put(email, new Horario(horario, dia));
+        }
+    }
+    
+    /**
+     * Adiciona um novo horario de atendimento a respectiva colecao
+     * @param email, String
+     * @param horario, String
+     * @param dia, String
+     */
+    public void addHorario(String email, String horario, String dia) {
+        this.horarios.get(email).addHorario(horario, dia);
+    }
+    /**
+     * Retorna por meio de boolean se esta cadastrado um horario de atendimento de um tutor cadastrado
+     * @param email, String
+     * @param horario, String
+     * @param dia, String
+     * @return boolean
+     */
+    public boolean contemHorario(String email, String horario, String dia) {
+        if (this.horarios.containsKey(email) && this.horarios.get(email).contemDia(dia)) {
+            return this.horarios.get(email).getHorario(dia).equals(horario);
+        } return false;
+        
+    }
+    /**
+     * Cadastra um novo local para atendimento de um tutor cadastrado
+     * @param email, String 
+     * @param local, String
+     */
+    public void cadastrarLocal(String email, String local) {
+        if (!this.serviceTutor.getAllTutores().contains(email)) {
+            throw new NullPointerException("Erro no cadastrar local de atendimento: tutor nao cadastrado");
+        }
+        else if (this.locaisAtendimento.containsKey(email)) {
+            this.locaisAtendimento.get(email).addLocal(local);
+        } else {
+            this.locaisAtendimento.put(email, new Local(local));
+        }
+    }
+    
+    /**
+     * Retorna se um local esta cadastrado para atendimento por meio de um boolean
+     * @param email, String
+     * @param local, String
+     * @return boolean
+     */
+    public boolean contemLocal(String email, String local) {
+        if (this.horarios.containsKey(email)) {
+            return this.locaisAtendimento.get(email).contemLocal(local);
+        } return false;
+    }
 }
--- src/servicos/ServiceTutor.java
+++ src/servicos/ServiceTutor.java
@@ -1,5 +1,11 @@
+/**
+ * UFCG - Universidade Federal de Campina Grande
+ * LP2 - Laboratorio de Programacao 2 - Projeto Final
+ * Caio Vitor Brasileiro Torres – 116111245
+ * Francivaldo Cabral de Andrade – 116111544
+ * Luan Carlos da Silva Bezerra - 116110100
+ */
 package servicos;
-
 import java.io.Serializable;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
@@ -7,101 +13,163 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import modelos.tutor.Disciplina;
 import modelos.tutor.Tutor;
-
+/**
+ * Classe ServiceCaixaSistema, realiza as operacoes no ambito do Caixa do Sistema
+ * @author Caio Torres
+ * @version 1.5
+ */
 @SuppressWarnings("serial")
 public class ServiceTutor implements Serializable {
-
-	private ServiceAluno serviceAluno;
-	private Map<String, Tutor> tutores;
-
-	public ServiceTutor(ServiceAluno serviceAluno) {
-		this.serviceAluno = serviceAluno;
-		this.tutores = new HashMap<>();
-	}
-
-	public void addTutor(String matricula, String disciplina, int proficiencia) {
-		if (!this.serviceAluno.containsAluno(matricula)) {
-			throw new NullPointerException("Erro na definicao de papel: Tutor nao encontrado");
-		} else if (this.tutores.containsKey(matricula)) {
-			this.tutores.get(matricula).addDisciplina(new Disciplina(disciplina, proficiencia));
-		} else {
-			this.tutores.put(matricula, new Tutor(matricula, this.serviceAluno.getInfoAluno(matricula, "Email"),
-			new Disciplina(disciplina, proficiencia), this.tutores.size()));
-		}
-	}
-
-	public Tutor getTutor(String matricula) {
-		if (this.tutores.containsKey(matricula)) {
-			return this.tutores.get(matricula);
-		}
-		return null;
-	}
-	
-	public String getToStringTutor(String matricula) {
-		if (this.tutores.containsKey(matricula)) {
-			return this.serviceAluno.toStringAluno(matricula);
-		}
-		return null;
-	}
-
-	public String getAllTutores() {
-		return this.serviceAluno.ordenacaoPersonalizada(this.tutores.keySet());
-	}
-
-	public boolean containsTutor(String matricula) {
-		return this.tutores.containsKey(matricula);
-	}
-	
-	public String searchTutor(String disciplina) {
-		String output = "";
-		for (Tutor tutor : this.tutores.values()) {
-			if (tutor.exibeDisciplinas().contains(disciplina))
-				output += tutor.getMatricula() + ",";
-		}
-		return output;
-	}
-	
-	public String getMatricula(String email) {
-		return this.serviceAluno.getMatricula(email);
-	}
-
-	public String melhorTutorAvaliado(String[] matriculas) {
-		List<Tutor> tutores = new ArrayList<>();
-		for (String matricula : matriculas) {
-			tutores.add(this.tutores.get(matricula));
-		}
-		Collections.sort(tutores);
-		return tutores.get(0).getMatricula();
-	}
-
-	public void avaliarTutor(int nota, String matricula) {
-		if (matricula == null) {
-			throw new NullPointerException("Erro na avaliacao de tutor: Ajuda nao atribuida a tutor");
-		} else {
-			this.tutores.get(matricula).addAvaliacao(nota);
-		}
-	}
-
-	public String getNota(String matricula) {
-		String saida = "";
-		DecimalFormat formato = new DecimalFormat("0.00");
-		saida = formato.format(this.tutores.get(matricula).getAvaliacao());
-		saida = saida.replace(".", ",");
-		return saida;
-	}
-
-	public String getNivel(String matricula) {
-		return this.tutores.get(matricula).getNivel();
-	}
-	
-	public double getTaxaAvaliacao(String matricula) {
-		return this.tutores.get(matricula).getTaxaAvaliacao();
-	}
-	
-	public void doar(String matricula, int valor) {
-		this.tutores.get(matricula).addDoacao(valor);
-	}
+    private ServiceAluno serviceAluno;
+    private Map<String, Tutor> tutores;
+    /**
+     * Construtor de ServiceTutor
+     * @param serviceAluno, ServiceAluno
+     */
+    public ServiceTutor(ServiceAluno serviceAluno) {
+        this.serviceAluno = serviceAluno;
+        this.tutores = new HashMap<>();
+    }
+    /**
+     * Adiciona um tutor na colecao de tutores
+     * @param matricula, String
+     * @param disciplina, String
+     * @param proficiencia, Integer
+     */
+    public void addTutor(String matricula, String disciplina, int proficiencia) {
+        if (!this.serviceAluno.containsAluno(matricula)) {
+            throw new NullPointerException("Erro na definicao de papel: Tutor nao encontrado");
+        } else if (this.tutores.containsKey(matricula)) {
+            this.tutores.get(matricula).addDisciplina(new Disciplina(disciplina, proficiencia));
+        } else {
+            this.tutores.put(matricula, new Tutor(matricula, this.serviceAluno.getInfoAluno(matricula, "Email"),
+            new Disciplina(disciplina, proficiencia), this.tutores.size()));
+        }
+    }
+    /**
+     * Retorna um Tutor cadastrado por meio da sua matricula
+     * @param matricula, String
+     * @return Tutor
+     */
+    public Tutor getTutor(String matricula) {
+        if (this.tutores.containsKey(matricula)) {
+            return this.tutores.get(matricula);
+        }
+        return null;
+    }
+    
+    /**
+     * Retorna a representacao textual de um Tutor cadastrado por meio da sua matricula
+     * @param matricula, String
+     * @return String
+     */
+    public String getToStringTutor(String matricula) {
+        if (this.tutores.containsKey(matricula)) {
+            return this.serviceAluno.toStringAluno(matricula);
+        }
+        return null;
+    }
+    /**
+     * Retorna a lista de tutores cadastrados, ordenados de uma maneira predefinida
+     * @return String
+     */
+    public String getAllTutores() {
+        return this.serviceAluno.ordenacaoPersonalizada(this.tutores.keySet());
+    }
+    /**
+     * Verifica se existe um tutor cadastrado por meio da sua matricula
+     * @param matricula, String
+     * @return boolean
+     */
+    public boolean containsTutor(String matricula) {
+        return this.tutores.containsKey(matricula);
+    }
+    
+    /**
+     * Retorna todas as matriculas dos tutores cadastrados
+     * @param disciplina, String
+     * @return String
+     */
+    public String searchTutor(String disciplina) {
+        String output = "";
+        for (Tutor tutor : this.tutores.values()) {
+            if (tutor.exibeDisciplinas().contains(disciplina))
+                output += tutor.getMatricula() + ",";
+        }
+        return output;
+    }
+    
+    /**
+     * Retorna a matricula de um tutor por meio do seu email
+     * @param email, String
+     * @return String
+     */
+    public String getMatricula(String email) {
+        return this.serviceAluno.getMatricula(email);
+    }
+    /**
+     * Retorna a matricula do melhor tutor avaliado
+     * @param matriculas, String[] matriculas
+     * @return String
+     */
+    public String melhorTutorAvaliado(String[] matriculas) {
+        List<Tutor> tutores = new ArrayList<>();
+        for (String matricula : matriculas) {
+            tutores.add(this.tutores.get(matricula));
+        }
+        Collections.sort(tutores);
+        return tutores.get(0).getMatricula();
+    }
+    /**
+     * Avalia um tutor
+     * @param nota, Integer
+     * @param matricula, String
+     */
+    public void avaliarTutor(int nota, String matricula) {
+        if (matricula == null) {
+            throw new NullPointerException("Erro na avaliacao de tutor: Ajuda nao atribuida a tutor");
+        } else {
+            this.tutores.get(matricula).addAvaliacao(nota);
+        }
+    }
+    /**
+     * Retorna a nota atual de um tutor
+     * @param matricula, String
+     * @return String
+     */
+    public String getNota(String matricula) {
+        String saida = "";
+        DecimalFormat formato = new DecimalFormat("0.00");
+        saida = formato.format(this.tutores.get(matricula).getAvaliacao());
+        saida = saida.replace(".", ",");
+        return saida;
+    }
+    /**
+     * Retorna o nivel atual do tutor, com base na sua nota atual
+     * @param matricula, String
+     * @return String
+     */
+    public String getNivel(String matricula) {
+        return this.tutores.get(matricula).getNivel();
+    }
+    
+    /**
+     * Retorna a taxa que sera cobrada sobre doacoes recebidas
+     * @param matricula, String
+     * @return double
+     */
+    public double getTaxaAvaliacao(String matricula) {
+        return this.tutores.get(matricula).getTaxaAvaliacao();
+    }
+    
+    /**
+     * Realiza uma doacao para um tutor especifico
+     * @param matricula, String
+     * @param valor, Integer
+     */
+    public void doar(String matricula, int valor) {
+        this.tutores.get(matricula).addDoacao(valor);
+    }
 }
